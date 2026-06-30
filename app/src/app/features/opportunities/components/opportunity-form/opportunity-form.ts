5b95c90c5e0083afb14afa51071295e2872af06b
Florian Meudec <florian.meudec.dev@outlook.com>
2026-06-30 13:18:51 +0200

    Opportunities - Opportunity Form - Reactive Form

--- app/src/app/features/opportunities/components/opportunity-form/opportunity-form.ts
+++ app/src/app/features/opportunities/components/opportunity-form/opportunity-form.ts
@@ -1,36 +1,88 @@
-import { Component, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { Modal } from '../../../../shared/components/modal/modal';
 import { SelectMenu } from '../../../../shared/components/select-menu/select-menu';
-import { SelectMenuOption } from '../../../../shared/models/select-menu-option.model';
+
+import { COMPANY_TYPES } from '../../../../shared/constants/company.constants';
+import { DURATION_UNITS } from '../../../../shared/constants/duration.constants';
+import { SENIORITIES } from '../../../../shared/constants/seniority.constants';
+import { WORK_MODALITIES } from '../../../../shared/constants/work-modality.constants';
+
+import { OPPORTUNITY_EVENT_TYPES } from '../../constants/opportunity.constants';
 
 @Component({
   selector: 'app-opportunity-form',
-
   standalone: true,
-
-  imports: [Modal, SelectMenu],
-
+  imports: [Modal, ReactiveFormsModule, SelectMenu],
   templateUrl: './opportunity-form.html',
-
   styleUrl: './opportunity-form.scss',
 })
 export class OpportunityForm {
   readonly closed = output<void>();
 
+  private readonly formBuilder = inject(NonNullableFormBuilder);
+
+  readonly noSelection = '';
+
+  readonly companyTypes = Object.values(COMPANY_TYPES);
+
+  readonly modalities = Object.values(WORK_MODALITIES);
+
+  readonly durationUnits = Object.values(DURATION_UNITS);
+
+  readonly seniorities = Object.values(SENIORITIES);
+
+  readonly nextActionTypes = Object.values(OPPORTUNITY_EVENT_TYPES).filter(
+    (type) => type.value !== OPPORTUNITY_EVENT_TYPES.STATUS_CHANGED.value,
+  );
+
+  readonly form = this.formBuilder.group({
+    companyName: ['', Validators.required],
+    companyType: [''],
+    industry: [''],
+    source: ['', Validators.required],
+
+    contactName: [''],
+    contactRole: [''],
+    contactEmail: ['', Validators.email],
+
+    missionTitle: ['', Validators.required],
+    description: [''],
+
+    tjm: [undefined as number | undefined],
+    workload: [undefined as number | undefined],
+
+    modality: [''],
+    location: [''],
+
+    estimatedStartDate: [''],
+
+    durationValue: [undefined as number | undefined],
+    durationUnit: [''],
+
+    seniority: [''],
+
+    stack: [''],
+
+    nextAction: this.formBuilder.group({
+      type: ['', Validators.required],
+      label: ['', Validators.required],
+      dueDate: ['', Validators.required],
+    }),
+  });
+
   requestClose(): void {
     this.closed.emit();
   }
 
-  readonly noSelection = '';
+  submit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
 
-  readonly companyTypes: SelectMenuOption[] = [];
+      return;
+    }
 
-  readonly modalities: SelectMenuOption[] = [];
-
-  readonly seniorities: SelectMenuOption[] = [];
-
-  readonly durationUnits: SelectMenuOption[] = [];
-
-  readonly nextActionTypes: SelectMenuOption[] = [];
+    console.log(this.form.getRawValue());
+  }
 }
