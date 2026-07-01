24b37b8351e845864aacc6a01e965ccdffadb5ff
Florian Meudec <florian.meudec.dev@outlook.com>
2026-07-01 11:54:02 +0200

    Opportunities - Opportunity Form - Submission

--- app/src/app/features/opportunities/components/opportunity-form/opportunity-form.ts
+++ app/src/app/features/opportunities/components/opportunity-form/opportunity-form.ts
@@ -9,7 +9,17 @@ import { DURATION_UNITS } from '../../../../shared/constants/duration.constants'
 import { SENIORITIES } from '../../../../shared/constants/seniority.constants';
 import { WORK_MODALITIES } from '../../../../shared/constants/work-modality.constants';
 
+import { CompanyType } from '../../../../shared/types/company.type';
+import { DurationUnit } from '../../../../shared/types/duration.type';
+import { Seniority } from '../../../../shared/types/seniority.type';
+import { WorkModality } from '../../../../shared/types/work-modality.type';
+
 import { OPPORTUNITY_EVENT_TYPES } from '../../constants/opportunity.constants';
+
+import { CreateOpportunityCommand } from '../../commands/create-opportunity.command';
+import { OpportunityMapper } from '../../mappers/opportunity.mapper';
+import { Opportunity } from '../../models/opportunity.model';
+import { OpportunityEventType } from '../../types/opportunity.type';
 
 @Component({
   selector: 'app-opportunity-form',
@@ -20,6 +30,8 @@ import { OPPORTUNITY_EVENT_TYPES } from '../../constants/opportunity.constants';
 })
 export class OpportunityForm {
   readonly closed = output<void>();
+
+  readonly created = output<Opportunity>();
 
   private readonly formBuilder = inject(NonNullableFormBuilder);
 
@@ -39,7 +51,7 @@ export class OpportunityForm {
 
   readonly form = this.formBuilder.group({
     companyName: ['', Validators.required],
-    companyType: [''],
+    companyType: this.formBuilder.control<CompanyType | ''>(''),
     industry: [''],
     source: ['', Validators.required],
 
@@ -52,21 +64,20 @@ export class OpportunityForm {
 
     tjm: [undefined as number | undefined],
     workload: [undefined as number | undefined],
-
-    modality: [''],
+    modality: this.formBuilder.control<WorkModality | ''>(''),
     location: [''],
 
     estimatedStartDate: [''],
 
     durationValue: [undefined as number | undefined],
-    durationUnit: [''],
+    durationUnit: this.formBuilder.control<DurationUnit | ''>(''),
 
-    seniority: [''],
+    seniority: this.formBuilder.control<Seniority | ''>(''),
 
     stack: [''],
 
     nextAction: this.formBuilder.group({
-      type: ['', Validators.required],
+      type: this.formBuilder.control<OpportunityEventType | ''>('', Validators.required),
       label: ['', Validators.required],
       dueDate: ['', Validators.required],
     }),
@@ -77,13 +88,53 @@ export class OpportunityForm {
   }
 
   submit(): void {
-    console.log(this.form.getRawValue());
     if (this.form.invalid) {
       this.form.markAllAsTouched();
 
       return;
     }
+    const command = this.createCommand();
+    const opportunity = OpportunityMapper.toOpportunity(command);
+    this.created.emit(opportunity);
+    this.requestClose();
+  }
 
-    console.log(this.form.getRawValue());
+  /*
+    Build the business command from the
+    current form values.
+  */
+  private createCommand(): CreateOpportunityCommand {
+    const value = this.form.getRawValue();
+
+    return {
+      companyName: value.companyName,
+      companyType: value.companyType || undefined,
+      industry: value.industry || undefined,
+      source: value.source,
+      contactName: value.contactName || undefined,
+      contactRole: value.contactRole || undefined,
+      contactEmail: value.contactEmail || undefined,
+      missionTitle: value.missionTitle,
+      description: value.description || undefined,
+      tjm: value.tjm,
+      workload: value.workload,
+      modality: value.modality || undefined,
+      location: value.location || undefined,
+      estimatedStartDate: value.estimatedStartDate || undefined,
+      durationValue: value.durationValue,
+      durationUnit: value.durationUnit || undefined,
+      seniority: value.seniority || undefined,
+
+      stack: value.stack
+        .split(',')
+        .map((technology) => technology.trim())
+        .filter((technology) => technology.length > 0),
+
+      nextAction: {
+        type: value.nextAction.type as OpportunityEventType,
+        label: value.nextAction.label,
+        dueDate: value.nextAction.dueDate,
+      },
+    };
   }
 }
